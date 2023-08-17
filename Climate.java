e7b8de230017903f766518e1b2147509783d1ed2
VELALA VENKATA SAKETH <119733812+velalasaketh@users.noreply.github.com>
2023-08-17 12:16:00 +0530

    import java.util.*; public class hr{     public static void main(String[] args)     {         int t;         Scanner h=new Scanner(System.in);         t=h.nextInt();         if(t>20)         {             System.out.println("HOT");         }         else         {             System.out.println("COLD");         }     } }

---
+++ Climate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class hr{
+    public static void main(String[] args)
+    {
+        int t;
+        Scanner h=new Scanner(System.in);
+        t=h.nextInt();
+        if(t>20)
+        {
+            System.out.println("HOT");
+        }
+        else
+        {
+            System.out.println("COLD");
+        }
+    }
+}
