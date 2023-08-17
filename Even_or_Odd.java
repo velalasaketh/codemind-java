78a9a545a4abd984e9f23bbe740d053042ab1336
VELALA VENKATA SAKETH <119733812+velalasaketh@users.noreply.github.com>
2023-08-17 11:05:28 +0530

    import java.util.*; public class hr {     public static void main(String args[])     {         int n;         Scanner h=new Scanner(System.in);         n=h.nextInt();         if(n%2==0)         {             System.out.println("Even");         }         else         {             System.out.println("Odd");         }     } }

---
+++ Even_or_Odd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class hr
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner h=new Scanner(System.in);
+        n=h.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else
+        {
+            System.out.println("Odd");
+        }
+    }
+}
