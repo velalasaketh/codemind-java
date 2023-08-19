ce5392ff4b042fe7907618733797dd32a5b2cf28
VELALA VENKATA SAKETH <119733812+velalasaketh@users.noreply.github.com>
2023-08-19 19:25:26 +0530

    import java.util.*; public class hr{     public static void main(String[] args)     {         int a,b;         Scanner h = new Scanner(System.in);         a=h.nextInt();         b=h.nextInt();         if(a>b)         {             System.out.printf("%d",a);         }         else         {             System.out.printf("%d",b);         }     } }

---
+++ Biggest_of_Two_Numbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class hr{
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner h = new Scanner(System.in);
+        a=h.nextInt();
+        b=h.nextInt();
+        if(a>b)
+        {
+            System.out.printf("%d",a);
+        }
+        else
+        {
+            System.out.printf("%d",b);
+        }
+    }
+}
