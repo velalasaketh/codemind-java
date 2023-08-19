8d0539f79f6ed437ca33d36b89fe0b1f85f0b437
VELALA VENKATA SAKETH <119733812+velalasaketh@users.noreply.github.com>
2023-08-19 19:29:11 +0530

    import java.util.*; public class hr{     public static void main(String[] args)     {         int n;         Scanner h=new Scanner(System.in);         n=h.nextInt();         if(n%3==0)         {             System.out.println("True");         }         else         {             System.out.println("False");         }     } }

---
+++ Number_Divisible_with_9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class hr{
+    public static void main(String[] args)
+    {
+        int n;
+        Scanner h=new Scanner(System.in);
+        n=h.nextInt();
+        if(n%3==0)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
