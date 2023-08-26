e30d7852f10189f1ccfcd23dc83374243e829b11
VELALA VENKATA SAKETH <119733812+velalasaketh@users.noreply.github.com>
2023-08-26 14:39:57 +0530

    import java.util.*; public class hr{     public static void main(String[] args)     {         String s;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();         System.out.println(s);         } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hr{
+    public static void main(String[] args)
+    {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.println(s);
+        }
+}
