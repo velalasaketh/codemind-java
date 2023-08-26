7b9afd663a23172db587e035e3f1f84c077a8e61
VELALA VENKATA SAKETH <119733812+velalasaketh@users.noreply.github.com>
2023-08-26 14:34:16 +0530

    import java.util.Scanner; public class hello {     public static void main(String args[])     {         int a;         int b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         System.out.println(a+b);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class hello
+{
+    public static void main(String args[])
+    {
+        int a;
+        int b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
