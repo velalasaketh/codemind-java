8fc1af8a9c94fe32fcb336871e3e66494df49744
VELALA VENKATA SAKETH <119733812+velalasaketh@users.noreply.github.com>
2023-08-26 14:20:12 +0530

    import java.util.*; public class hr{     public static void main(String[] args)     {         Scanner h = new Scanner(System.in);         int a,b,c;         a=h.nextInt();         b=h.nextInt();         c=a/b;         System.out.printf("%d",c);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class hr{
+    public static void main(String[] args)
+    {
+        Scanner h = new Scanner(System.in);
+        int a,b,c;
+        a=h.nextInt();
+        b=h.nextInt();
+        c=a/b;
+        System.out.printf("%d",c);
+    }
+}
